3853595886b291e36726bbc40d4fb1e5e5afbe7b
Deepak Kendole <deepakdpk101@gmail.com>
2022-03-04 01:14:40 +0530

    Request Header Parser Microservice

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 My Project hosted on replit.com
 1) Timestamp Microservice
 https://replit.com/@DeepakKendole/boilerplate-project-timestamp#server.js
+
+2) Request Header Parser Microservice
+https://replit.com/@DeepakKendole/boilerplate-project-headerparser#s/%40types/express-serve-static-core/ts4.0/index.d.ts:385:4
--- server.js
+++ server.js
@@ -52,6 +52,17 @@ app.get("/api/:date?", function(req, res) {
   res.json(response);
 })
 
+app.get("/api/whoami", function(req, res) {
+  const ipaddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
