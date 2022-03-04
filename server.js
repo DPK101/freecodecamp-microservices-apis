c81d0274a64fc88beb871ba136aac4b9a7ca8756
Deepak Kendole <deepakdpk101@gmail.com>
2022-03-04 07:00:28 +0530

    Refactor: Request Header Parser Microservice

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function(req, res) {
+  const ipaddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
+
 app.get("/api/:date?", function(req, res) {
   let unixMilliseconds;
   let utcDate;
@@ -50,17 +61,6 @@ app.get("/api/:date?", function(req, res) {
     response = { error: "Invalid Date" };
   }
   res.json(response);
-})
-
-app.get("/api/whoami", function(req, res) {
-  const ipaddress = req.socket.remoteAddress;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-  res.json({
-    ipaddress,
-    language,
-    software
-  })
 })
 
 // listen for requests :)
